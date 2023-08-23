57a73f85af4a78bb00bb36646176b75da0fc9ffd
Youngwook Choi <duddnr3618@gmail.com>
2023-08-23 20:04:28 +0900

    23.08.23 TodoList03 실습

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
+
+
+// 라이브러리 2개 설치 : icons , styled-component
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
+function App() {
+  return (
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
+  );
+}
+
+export default App;
